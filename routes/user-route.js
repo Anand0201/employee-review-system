9747165a8135b4509e1c4467e81fa9b91ce4da24
hakthon <anandjethava538@gmail.com>
2024-10-31 11:39:50 +0530

    Commit

--- routes/user-route.js
+++ routes/user-route.js
@@ -7,6 +7,6 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage })
 
 userrouter.post('/add_employee', upload.fields([{ name: 'personalid', maxCount: 1 }, { name: 'resume', maxCount: 1 }, { name: 'passport', maxCount: 1}]), signupController);
-userrouter.post('/create_project', upload.fields([{ name: 'projectdocument', maxCount: 1}], createProject);
+userrouter.post('/create_project', upload.fields([{ name: 'projectdocument', maxCount: 1}], createProject));
 
 export default userrouter;
